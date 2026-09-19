c8fac6a42d7bedb568995527e8df565dce057ff6
monayaw <shofimaulanashfa@gmail.com>
2026-09-19 13:27:26 +0700

    dosen pembimbing

--- app/Views/mahasiswa/index.php
+++ app/Views/mahasiswa/index.php
@@ -12,35 +12,64 @@
 
 <body>
     <div class="container mt-5">
+
         <h1 class="text-center mb-4">Politeknik Negeri Jember</h1>
+
         <h2 class="mb-4">Daftar Mahasiswa</h2>
+
         <div class="table-responsive">
+
             <table class="table table-bordered table-striped">
+
                 <thead class="table-dark">
                     <tr>
                         <th>NIM</th>
                         <th>Nama</th>
                         <th>Program Studi</th>
+                        <th>Dosen Pembimbing</th>
                         <th>Aksi</th>
                     </tr>
                 </thead>
 
                 <tbody>
+
                     <?php foreach ($mahasiswa as $mhs): ?>
+
                         <tr>
-                            <td><?= $mhs['nim']; ?></td>
-                            <td><?= $mhs['nama']; ?></td>
-                            <td><?= $mhs['prodi']; ?></td>
+
                             <td>
-                                <a href="/si-akademik/public/mahasiswa/detail?nim=<?= $mhs['nim']; ?>" class="btn btn-primary btn-sm">
+                                <?= htmlspecialchars($mhs['nim']); ?>
+                            </td>
+
+                            <td>
+                                <?= htmlspecialchars($mhs['nama']); ?>
+                            </td>
+
+                            <td>
+                                <?= htmlspecialchars($mhs['prodi']); ?>
+                            </td>
+
+                            <td>
+                                <?= htmlspecialchars($mhs['nama_dosen'] ?? 'Belum ada dosen'); ?>
+                            </td>
+
+                            <td>
+                                <a href="/si-akademik/public/mahasiswa/detail?nim=<?= $mhs['nim']; ?>"
+                                    class="btn btn-primary btn-sm">
                                     Detail
                                 </a>
                             </td>
+
                         </tr>
+
                     <?php endforeach; ?>
+
                 </tbody>
+
             </table>
+
         </div>
+
     </div>
 </body>
 
